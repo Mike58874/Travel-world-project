8549f76db8a1bf452da07b3203fc1c338f9aae83
Mike58874 <michael.lim@students.mikroskil.ac.id>
2026-07-02 21:42:48 +0700

    Final

--- jss/loginscript.js
+++ jss/loginscript.js
@@ -1,12 +1,9 @@
-// Tangani submit form login
 const formLogin = document.getElementById('form-login');
 
 formLogin.addEventListener('submit', function (event) {
-    event.preventDefault(); // mencegah refresh halaman
+    event.preventDefault();
 
-    // TODO: tambahkan validasi / pengecekan akun di sini
     alert('Login berhasil!');
 
-    // contoh redirect setelah login sukses
     window.location.href = 'index.html';
 });
